34c8999230b7fac64902b91b625d252d79d18b2b
Byron Jones <byron@glob.com.au>
2025-02-25 14:07:51 +0800

    truncate slack text to 3000 chars

--- src/slack.py
+++ src/slack.py
@@ -11,6 +11,15 @@ def _escape(value: str) -> str:
 
 
 def notify(url: str, post: Post) -> None:
+    # text has a limit of 3000 characters
+    header = f"*<{post.link}|{_escape(post.title)}>*\n\n"
+    text = f"{header}{_escape(post.content)}"
+    if len(text) > 3000:
+        length = 3000 - len(header) - 3 + 5
+        while len(text) > 3000:
+            length -= 5
+            text = f"{header}{_escape(post.content[:length])}..."
+
     payload = {
         "text": _escape(post.title),
         "blocks": [
@@ -18,10 +27,7 @@ def notify(url: str, post: Post) -> None:
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": (
-                        f"*<{post.link}|{_escape(post.title)}>*\n\n"
-                        f"{_escape(post.content)}"
-                    ),
+                    "text": text,
                 },
                 "accessory": {
                     "type": "image",
@@ -42,6 +48,15 @@ def notify(url: str, post: Post) -> None:
     try:
         req = url_request.Request(url, method="POST", data=json.dumps(payload).encode())
         url_request.urlopen(req)
+    except url_request.HTTPError as e:
+        # transient error, bail out but don't treat this as an error
+        logger.info(f"failed to post to slack: {e}")
+        try:
+            logger.info(e.read().decode())
+        except Exception:  # noqa BLE001
+            pass
+        logger.info(json.dumps(payload, indent=2, sort_keys=True))
+        sys.exit(0)
     except OSError as e:
         # transient error, bail out but don't treat this as an error
         logger.info(f"failed to post to slack: {e}")
